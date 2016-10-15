12429bd7ea99a835085bf4e37452501e94dff445
ericmvaughn <eric_vaughn@yahoo.com>
2016-10-14 21:31:20 -0400

    Get all of the tests working

--- test/test.js
+++ test/test.js
@@ -195,78 +195,83 @@ describe('Blockchain REST interface', function() {
   });
 });
 
+// dummy hash values for testing
+var hash1 = 'hash1-1234567890123456789012345678901234567890123456789012345678'
+var hash2 = 'hash2-1234567890123456789012345678901234567890123456789012345678'
+
 //testing the Application REST interface that uses the node SDK
 describe('Application REST interface', function() {
 
   describe('/addDoc', function() {
-    it('should add a document to the block chain', function(done) {
-      this.timeout(4000);
+    it('should add a document to the blockchain', function(done) {
+      this.timeout(6000);
       request
       .post(url + '/addDoc')
-      .send({hash: 'abcd1', name: 'dummyFile1.txt', owner: 'Alice', hashType: 'sha256'})
+      .send({hash: hash1, name: 'dummyFile1.txt', owner: 'Alice', hashType: 'sha256'})
       .end(function(err, res) {
       assert.isNull(err);
-      //console.log(res.body);
-      assert.property(res.body, 'uuid', 'reply should have uuid');
+      // console.log(res.body);
+      assert.property(res.body, 'result', 'reply should have result');
+      assert.isString(res.body.result, 'the result should be a string');
       done();
       });
     });
     it('should add a second document', function(done) {
-      this.timeout(4000);
+      this.timeout(6000);
       request
       .post(url + '/addDoc')
-      .send({hash: 'abcd2', name: 'dummyFile2.txt', owner: 'Bob', hashType: 'sha256'})
+      .send({hash: hash2, name: 'dummyFile2.txt', owner: 'Bob', hashType: 'sha256'})
       .end(function(err, res) {
       assert.isNull(err);
       //console.log(res.body);
-      assert.property(res.body, 'uuid', 'reply should have uuid');
-      setTimeout(function() {
-        done();
-      }, 2000);    // hack to give time for the transaction to complete
+      assert.property(res.body, 'result', 'reply should have result');
+      assert.isString(res.body.result, 'the result should be a string');
+      done();
       });
     });
   });
   describe('/listDoc', function(){
     it('should return a list of users', function(done) {
-      this.timeout(4000);
+      this.timeout(6000);
       request
       .get(url + '/listDoc')
       .accept('application/json')
       .end(function(err, res) {
         assert.isNull(err);
-        //console.log(res.body);
-        assert.property(res.body, 'abcd1', 'Document hash abcd1 should be in the list');
-        assert.property(res.body.abcd1, 'owner', 'Document owner should be present');
-        assert.equal(res.body.abcd1.owner, 'Alice', 'the owner should be Alice');
+        // console.log(res.body);
+        assert.property(res.body, hash1, 'hash1 should be in the list');
+        var doc = JSON.parse(res.body[hash1]);
+        assert.property(doc, 'Owner', 'Document owner should be present');
+        assert.equal(doc.Owner, 'Alice', 'the owner should be Alice');
         done();
       });
     });
   });
   describe('/transferDoc', function() {
     it('should transfer doc from Alice to Bob', function(done) {
-      this.timeout(4000);
+      this.timeout(6000);
       request
-      .post(url + '/transferDoc')
-      .send({hash: 'abcd1', toOwner: 'Bob'})
+      .post(url + '/editDoc')
+      .send({hash: hash1, owner: 'Bob'})
       .end(function(err, res) {
         assert.isNull(err);
         //console.log(res.body);
-        assert.property(res.body, 'uuid', 'reply should have uuid');
-        setTimeout(function() {
-          done();
-        }, 2000);    // hack to give time for the transaction to complete
+        assert.property(res.body, 'result', 'reply should have result');
+        assert.isString(res.body.result, 'the result should be a string');
+        done();
       });
     });
     it('Bob should be the new owner', function(done) {
-      this.timeout(4000);
+      this.timeout(6000);
       request
       .get(url + '/listDoc')
       .end(function(err, res) {
         assert.isNull(err);
-        //console.log(res.body);
-        assert.property(res.body, 'abcd1', 'Document hash abcd1 should be in the list');
-        assert.property(res.body.abcd1, 'owner', 'Document owner should be present');
-        assert.equal(res.body.abcd1.owner, 'Bob', 'the owner should be Bob');
+        // console.log(res.body);
+        assert.property(res.body, hash1, 'hash1 should be in the list');
+        var doc = JSON.parse(res.body[hash1]);
+        assert.property(doc, 'Owner', 'Document owner should be present');
+        assert.equal(doc.Owner, 'Bob', 'the owner should be Bob');
         done();
       });
     });
@@ -274,31 +279,28 @@ describe('Application REST interface', function() {
 
   describe('/verifyDoc', function() {
     it('should be able to verify the document exists', function(done) {
-      this.timeout(4000);
+      this.timeout(6000);
       request
-      .get(url + '/verifyDoc/' + 'abcd1')
+      .get(url + '/verifyDoc/' + hash1)
       .end(function(err, res) {
         assert.isNull(err);
         assert.equal(res.status, 200, 'should recieve a 200 status');
-        assert.property(res.body, 'errorCode', 'response should have an errorCode');
-        assert.equal(res.body.errorCode, 0, 'should return a 0 for correctly verifing the balance');
-        assert.property(res.body, 'results', 'response should have results');
-        assert.property(res.body.results, 'hash', 'return value should contain the hash');
-        assert.equal(res.body.results.hash, 'abcd1', 'the hash should be the same as the input hash');
+        // console.log(res.body);
+        assert.property(res.body, 'Hash', 'response should have a Hash');
+        assert.equal(res.body.Hash, hash1, 'the hash should be the same as the input hash');
         done();
       });
     });
     it('should report an error for an unknown hash value', function(done) {
-      this.timeout(4000);
+      this.timeout(6000);
       request
       .get(url + '/verifyDoc/' + 'abcd1234')
       .end(function(err, res) {
-        assert.isNull(err);
-        assert.equal(res.status, 200, 'should recieve a 200 status');
-        assert.property(res.body, 'errorCode', 'response should have an errorCode');
-        assert.equal(res.body.errorCode, 1, 'should return a 1 if the hash is not on the blockchain');
-        assert.property(res.body, 'results', 'response should have results');
-        assert.equal(res.body.results, 'Document not found', 'should return document not found');
+        // console.log(res);
+        assert.isNotNull(err);
+        assert.equal(res.status, 500, 'should recieve a 500 status');
+        assert.property(res, 'text', 'response should have a text field');
+        assert.equal(res.text, 'Document not found', 'should return document not found');
         done();
       });
     });
@@ -306,40 +308,40 @@ describe('Application REST interface', function() {
   });
   describe('/delDoc', function() {
     it('should delete a test document', function(done) {
-      this.timeout(4000);
+      this.timeout(6000);
       request
       .post(url + '/delDoc')
-      .send({hash: 'abcd1'})
+      .send({hash: hash1})
       .end(function(err, res) {
       assert.isNull(err);
       //console.log(res.body);
-      assert.property(res.body, 'uuid', 'reply should have uuid');
+      assert.property(res.body, 'result', 'reply should have result');
+      assert.isString(res.body.result, 'the result should be a string');
       done();
       });
     });
     it('should delete second test document', function(done) {
-      this.timeout(4000);
+      this.timeout(6000);
       request
       .post(url + '/delDoc')
-      .send({hash: 'abcd2'})
+      .send({hash: hash2})
       .end(function(err, res) {
       assert.isNull(err);
       //console.log(res.body);
-      assert.property(res.body, 'uuid', 'reply should have uuid');
-      setTimeout(function() {
-        done();
-      }, 2000);    // hack to give time for the transaction to complete
+      assert.property(res.body, 'result', 'reply should have result');
+      assert.isString(res.body.result, 'the result should be a string');
+      done();
       });
     });
     it('both test documents should be gone', function(done) {
-      this.timeout(4000);
+      this.timeout(6000);
       request
       .get(url + '/listDoc')
       .end(function(err, res) {
         assert.isNull(err);
         //console.log(res.body);
-        assert.notProperty(res.body, 'abcd1', 'abcd1 should not be in the list');
-        assert.notProperty(res.body, 'abcd2', 'abcd2 should be not in the list');
+        assert.notProperty(res.body, hash1, 'hash1 should not be in the list');
+        assert.notProperty(res.body, hash2, 'hash2 should be not in the list');
         done();
       });
     });
@@ -348,7 +350,7 @@ describe('Application REST interface', function() {
 
 describe('Blockchain utilities', function() {
   describe('updateChain()', function() {
-    it('updateChain should update the chain height', function(done) {
+    it('should update the chain height', function(done) {
       this.timeout(30000);
       //var origHeight = util.updateChain();
       var origHeight = 0;
@@ -363,28 +365,39 @@ describe('Blockchain utilities', function() {
       // issue a dummy transfer to create a new block
       request
       .post(url + '/addDoc')
-      .send({hash: '1234', name: 'dummyFile.txt', owner: 'Alice', hashType: 'sha256'})
+      .send({hash: hash1, name: 'dummyFile.txt', owner: 'Alice', hashType: 'sha256'})
       .end(function(err, res) {
         assert.isNull(err);
-        setTimeout(function() {
-          var newHeight = 0;
-          util.updateChain(newHeight).then(function(height){
-            //console.log('the newHeight is ' + height);
-            newHeight = height;
-            assert.equal(newHeight, origHeight + 1,
-              'the blockchain height should increase by 1');
+        var newHeight = 0;
+        util.updateChain(newHeight).then(function(height){
+          //console.log('the newHeight is ' + height);
+          newHeight = height;
+          assert.equal(newHeight, origHeight + 1,
+            'the blockchain height should increase by 1');
+          done();
+          }, function(response) {
+            console.log(response);
+            console.log('Error updating the chain height ' + response.error);
+            assert.isNull(response);
             done();
-            }, function(response) {
-              console.log(response);
-              console.log('Error updating the chain height ' + response.error);
-              assert.isNull(response);
-              done();
-            });
-          }, 2000);    // hack to give time for the transaction to complete
+          });
         });
+      });
+      it('then cleans up the test doc with deleteDoc', function(done) {
+        this.timeout(6000);
+        request
+        .post(url + '/delDoc')
+        .send({hash: hash1})
+        .end(function(err, res) {
+        assert.isNull(err);
+        //console.log(res.body);
+        assert.property(res.body, 'result', 'reply should have result');
+        assert.isString(res.body.result, 'the result should be a string');
+        done();
       });
     });
   });
+});
 
 describe('Block list utilities', function() {
   describe('initialBlockList', function() {
